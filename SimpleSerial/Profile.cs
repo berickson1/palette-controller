26acd870bce6d58474c0f5ef68773030c1d9d6e3
Julia <julia.chernushevich@gmail.com>
2013-03-12 22:15:45 -0400

    added serial tester

---
+++ SimpleSerial/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Profile
+{
+    int ID;
+    string name;
+    List<Module> moduleList = new List<Module>();
+
+    public Profile(int id, string nm, List<Module> mL)
+    {
+        ID = id;
+        name = nm;
+        moduleList = mL;
+    }
+    //other functions
+}
